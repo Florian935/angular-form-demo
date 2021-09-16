260c74f344173cb79b48c8fe76a0e3154f733a97
florian935 <florian.martin63000@gmail.com>
2021-09-16 15:26:34 +0200

    Feat: better capitalize pipe coding

--- src/app/shared/custom-title-case.pipe.ts
+++ src/app/shared/custom-title-case.pipe.ts
@@ -5,6 +5,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CustomTitleCasePipe implements PipeTransform {
     transform(value: string): string {
-        return `${value.charAt(0).toUpperCase()}${value.slice(1)}`;
+        return `${value[0].toUpperCase()}${value.slice(1)}`;
     }
 }
