985ffb87d368b4777fb6c095681124e7f5c00ad5
florian935 <florian.martin63000@gmail.com>
2021-09-17 12:02:37 +0200

    Feat: added debounce time and first operator to have a finite Observable

--- src/app/shared/directive/username-exist.directive.ts
+++ src/app/shared/directive/username-exist.directive.ts
@@ -3,13 +3,24 @@ import {
     AsyncValidatorFn,
     ValidationErrors,
 } from '@angular/forms';
-import { catchError, map, Observable, of } from 'rxjs';
+import {
+    catchError,
+    debounceTime,
+    first,
+    map,
+    Observable,
+    of,
+    tap,
+} from 'rxjs';
 import { UserService } from 'src/app/core/services/user.service';
 
 export function usernameExist(userService: UserService): AsyncValidatorFn {
     return (control: AbstractControl): Observable<ValidationErrors | null> => {
         return userService.checkifUsernameExists(control.value).pipe(
+            debounceTime(2000),
+            first(),
             map((res) => {
+                console.log('CALL');
                 return res ? { usernameExist: true } : null;
             }),
             catchError(() => of(null))
